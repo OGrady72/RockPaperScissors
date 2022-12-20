687c711fd9c5dba431a699af2c0a83bee097e787
Chris Scollin <kcscollin@gmail.com>
2022-12-19 17:28:34 -0700

    Added PlayRound() method which plays one round of the game.

--- js/rps.js
+++ js/rps.js
@@ -1,43 +1,39 @@
-// getComputerChoice()
-// Setup an ComputerChoice array with three variable, "Rock, Paper, Scissors"
-// create a RandomNumber variable.
-// Assign the value of a Random number between 0 and 2 to the randomNumber variable.
-// return the value of the ComputerChoice[Random] number. 
 
-// getPlayerChoice()
-// create Choice variable to hold value user chooses
-// Prompt the user for a choice "Rock, Paper, Scissors"
-// assign user choice to the choice variable.
-// RegEx test against "Rock, Paper, or Scissors case insensitive"
-// If test fails.. Prompt user again.
-// Make a new string all lowercase.
-// Make a new String first letter uppercase.
-// IF not, Invalid Choice, Prompt the user again.
-// Return Valid Player Choice.
 
 let player = getPlayerChoice();
 let comp = getComputerChoice();
 
-console.log(`computer choice is ${comp}`);
-console.log(`player choice is ${player}`);
+console.log(player);
 
-function getPlayerChoice() 
-{
-let regEx = /(rock|paper|scissors)/i;
-let loop = false;
-let choice = "";
+console.log(playRound(player, comp));
 
-do {
+// Get's the player choice and modifyies the string to "Rock, Paper, or Scissors" if the case 
+// does not match.
+// Returns the string "Rock", "Paper", or "Scissors".
+
+function getPlayerChoice() {
+
+  let regEx = /(rock|paper|scissors)/i;
+  let loop = false;
+  let choice = "";
+
+  do {
     choice = prompt(`Which do you choose, "Paper, Scissors or Rock"?`);
       loop = regEx.test(choice);
-} while (loop == false);
+  } while (loop == false);
 
-let first = choice.slice(0,1);
-let rest = choice.slice(1, (choice.length));
-let upper = first.toUpperCase();
-let answer = upper + rest;
+  if (choice == `Rock` || choice == `Paper` || choice == `Scissors`) {
+  return choice;
 
-return answer;
+} else {
+  let first = choice.slice(0,1);
+  let rest = choice.slice(1, (choice.length));
+  rest = rest.toLowerCase();
+  let upper = first.toUpperCase();
+  let answer = upper + rest;
+
+  return answer;
+  }
 }
 
 /* This function defines an array of choices "Rock, Paper, Scissors"
@@ -58,3 +54,70 @@ return compChoice;
 function getRandomNumber() {
   return Math.floor(Math.random() * 3);
 }
+
+// This is the playRound() function. It has two parameters, the userchoice
+// and the computerchoice. It plays a round of the game and returns a string
+// stating wheather you won or lost and what the values were. 
+
+function playRound(playerChoice, computerChoice) {
+
+  let rpsString ="";
+
+  if (playerChoice == "Rock") {
+
+    switch(computerChoice) {
+
+      case `Paper`:
+        rpsString = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        break;
+
+      case `Scissors`: 
+      rpsString = `You Win! ${playerChoice} beats ${computerChoice}`
+      break;
+
+      default: 
+      rpsString = `Tie! ${playerChoice} ties ${computerChoice}`;
+      break;
+    } 
+
+    return rpsString;
+  
+  } else if(playerChoice == "Scissors") {
+ 
+    switch(computerChoice) {
+
+      case `Paper`:
+        rpsString = `You Win! ${playerChoice} beats ${computerChoice}`;
+        break;
+
+      case `Rock`: 
+      rpsString = `You Lose! ${computerChoice} beats ${playerChoice}`;
+      break;
+
+      default: 
+      rpsString = `Tie! ${playerChoice} ties ${computerChoice}`;
+      break;
+    } 
+    return rpsString;
+  
+  } else {
+
+    switch(computerChoice) {
+
+      case `Rock`:
+        rpsString = `You Win! ${playerChoice} beats ${computerChoice}`;
+        break;
+
+      case `Scissors`: 
+      rpsString = `You Lose! ${computerChoice} beats ${playerChoice}`;
+      break;
+
+      default: 
+      rpsString = `Tie! ${playerChoice} ties ${computerChoice}`;
+      break;
+    } 
+    return rpsString;
+      
+    }
+  }
+ 
